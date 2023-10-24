39c297a242926f17cea9be0cf0d9a89f19252354
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-10-24 11:42:47 -0300

    Update Aula 2.java

--- Aula 2.java
+++ Aula 2.java
@@ -1,1 +1,22 @@
+package aulasjava;
 
+import java.util.Scanner;
+
+public class Aulas {
+
+	public static void main(String[] args) {
+		 @SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		 double nota = sc.nextDouble();
+		 System.out.println("Digite a nota: ");
+		 if(nota > 5) {
+			 System.out.println("Você foi aprovado!");
+			 
+		 } else {
+			 
+			 System.out.println("Você foi reprovado!");
+		 }
+		
+	}
+
+}
