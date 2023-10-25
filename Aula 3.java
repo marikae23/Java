e2cfdb2e362821ad7ce9547054d18c72fde8c4b3
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-10-25 11:40:36 -0300

    Create Aula 3.java

---
+++ Aula 3.java
@@ -0,0 +1,19 @@
+package aulas;
+
+public class Aulasjava {
+
+	public static void main(String[] args) {
+		int numero = 10;
+		if(eMultiploDe2(numero)) {
+			System.out.println(numero + " é um multiplo de 2");
+		} else {
+			System.out.println(numero + " não é múltiplo de 2");
+		}
+
+	}
+	
+	public static boolean eMultiploDe2(int numero) {
+		return numero %2 == 0;
+	}
+
+}
