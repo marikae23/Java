51ba161b3b7bb7e44ae88e1ef9227bb684556097
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-10-27 19:40:27 -0300

    Update Atividade 3.java

--- Atividade 3.java
+++ Atividade 3.java
@@ -25,3 +25,47 @@ public class CalculadoraNumeros {
         return Math.abs(num);
     }
 }
+
+___________________________________________________
+
+import java.util.Scanner;
+
+public class RemoverVogais {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma frase ou palavra: ");
+        String input = scanner.nextLine();
+        String resultado = removerVogais(input);
+        System.out.println("Resultado após remover as vogais: " + resultado);
+    }
+
+    public static String removerVogais(String input) {
+        String resultado = input.replaceAll("[aeiouAEIOU]", "");
+        return resultado;
+    }
+}
+
+___________________________________________________
+
+public class CalculaMedia {
+    public static void main(String[] args) {
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+        double nota3 = 6.5;
+        char tipoMedia = 'A';
+
+        double media = calcularMedia(nota1, nota2, nota3, tipoMedia);
+
+        System.out.println("A média do aluno é: " + media);
+    }
+
+    public static double calcularMedia(double nota1, double nota2, double nota3, char tipoMedia) {
+        if (tipoMedia == 'A' || tipoMedia == 'a') {
+        } else if (tipoMedia == 'P' || tipoMedia == 'p') {
+            return (5 * nota1 + 3 * nota2 + 2 * nota3) / (5 + 3 + 2);
+        } else {
+            System.out.println("Tipo de média inválido. Use 'A' para média aritmética ou 'P' para média ponderada.");
+            return 0.0;
+        }
+    }
+}
