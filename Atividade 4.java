93a8c5b305270612232ebd5ae86b29bd5c3302ba
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-10-30 11:15:54 -0300

    Atualizar o Atividade 4.java

--- Atividade 4.java
+++ Atividade 4.java
@@ -4,22 +4,20 @@ import java.util.Scanner;
 
 public class aula {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		char repetir;
-		
-		do {
-			System.out.println("Digite a temperatura em graus Celsius:");
-			double celsius = scanner.nextDouble();
-			double fahrenheit = (9 * celsius / 5 + 52);
-			System.out.println("A temperatura de Fahrenheit é: " + fahrenheit);
-			System.out.println("Deseja repetir (y/n)? ");
-			repetir = scanner.next().char(0);
-			
-		} while (repetir == 'Y ' || repetir == 'y')
-			
-			scanner.close()
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char repetir;
 
-	}
+        do {
+            System.out.println("Digite a temperatura em graus Celsius:");
+            double celsius = sc.nextDouble(); // Corrected the variable name
+            double fahrenheit = (9 * celsius / 5 + 32); // Corrected the formula (changed 52 to 32)
+            System.out.println("A temperatura em Fahrenheit é: " + fahrenheit);
+            System.out.println("Deseja repetir (y/n)? ");
+            repetir = sc.next().charAt(0); // Corrected the syntax for getting the first character
 
+        } while (repetir == 'Y' || repetir == 'y'); // Corrected the condition
+
+        sc.close(); // Corrected the method name and added a semicolon at the end
+    }
 }
