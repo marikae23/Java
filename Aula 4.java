be9309baa7c2701e76a49799e432fd29eafabb36
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-10-30 11:52:08 -0300

    Update Aula 4.java

--- Aula 4.java
+++ Aula 4.java
@@ -1,1 +1,34 @@
+package teste;
+import java.util.Locale;
+import java.util.Scanner;
 
+public class aula {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a ordem da matriz: ");
+        int n = sc.nextInt();
+        int[][] matriz = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("Digite o número: ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Matriz digitada:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " ");
+                
+            }
+            System.out.println();
+            
+            sc.close();
+           
+        }
+    }
+}
