8369ff852ea1909d037b8e35f93418bd146dddaa
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-11-01 12:36:27 -0300

    Atualizar o Policia e ladrao 19.java

--- Policia e ladrao 19.java
+++ Policia e ladrao 19.java
@@ -1,15 +1,71 @@
-Marcos é um ladrão reconhecido no mundo todo. Suas estratégias de fuga são tão boas que ele nunca foi pego pela polícia. Porém, ao saber que ele irá viajar para o Texas, interior de São Paulo, a polícia local planejou uma estratégia infalível para capturá-lo. Marcos sabe que a polícia do   Texas  é bem mais esperta que a de outros países, mas mesmo assim é seu sonho de infância visitar o país; ainda que ele seja pego, sua estratégia será maximizar o tempo que fica livre no país.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-A estratégia da polícia do   Texas  para capturar Marcos é bloquear o máximo de rodovias possível. No entanto, como eles não querem atrapalhar os moradores, precisam deixar algumas rodovias livres, de forma que ainda seja possível ir de qualquer cidade para qualquer outra usando apenas as rodovias livres.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-Por precaução, a polícia já bloqueou as rodovias antes que Marcos chegue ao país, e Marcos também já descobriu quais são as N−1 rodovias que estarão livres, sendo N o número de cidades do país. Mas como Marcos não sabe em qual cidade está a polícia, nem em qual cidade ele estará quando apolícia identificar que ele está no país, ele escreveu um programa que dadas as rodovias livres e Q pares de vértices, que representam onde a polícia estará e onde ele estará quando for identificado, responde qual o máximo de tempo que a fuga de Marcos durará caso ele e a polícia usem estratégias ótimas.
+        System.out.print("Digite o número de cidades e o número de consultas (N Q): ");
+        int N = sc.nextInt(); // Número de cidades
+        int Q = sc.nextInt(); // Número de consultas
 
-Marcos considera que tanto ele quanto a polícia levam sempre uma hora para ir de uma cidade para outra vizinha (por uma rodovia livre). Além disso, devido a forma como eles identificam em qual cidade o outro está, Marcos considera que ele age primeiro, depois a polícia age, depois ele age novamente e assim sucessivamente em turnos. Ele considera que em um turno se pode ficar parado por uma hora na mesma cidade, ou mover para uma outra cidade vizinha. Marcos também considera que ele será preso no primeiro momento que a polícia estiver na mesma cidade que ele (note que a polícia sempre consegue prender Marcos, e em todos os momentos tanto Marcos sabe onde a polícia está quanto a polícia sabe onde Marcos está).
+        if (N > 1000 || Q > 1000) {
+            System.out.println("Valores fora dos limites para pontuação máxima.");
+            return;
+        }
 
-Como Marcos é seu amigo e sabe que você programa bem, ele deseja que você programe uma solução para ele comparar com a dele.
+        List<List<Integer> > graph = new ArrayList<>(N);
 
-Entrada
-A primeira linha da entrada é composta por dois inteiros N e Q, que representam o número de cidades do Texas e o número de consultas. As N−1 linhas seguintes contém dois números inteiros cada, Ai​ e Bi​, representando que há uma rodovia livre que liga as cidades Ai​ e Bi​. As próximas Q linhas contém dois números inteiros cada, Ci​ e Di​, representando uma consulta onde a polícia estará na cidade Ci​ e Marcos na cidade Di​ no momento que eles identificam onde o outro está.
+        for (int i = 0; i < N; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-Saída
-A saída deverá ser composta por apenas um número inteiro, o tempo máximo que a fuga de Marcos durará caso ele e a polícia usem estratégias ótimas.
+        System.out.println("Digite as rodovias livres (A B):");
+        for (int i = 0; i < N - 1; i++) {
+            int Ai = sc.nextInt();
+            int Bi = sc.nextInt();
+
+            graph.get(Ai - 1).add(Bi);
+            graph.get(Bi - 1).add(Ai);
+        }
+
+        System.out.println("Digite as consultas (C D):");
+        for (int q = 0; q < Q; q++) {
+            int Ci = sc.nextInt();
+            int Di = sc.nextInt();
+
+            if (Ci != 1) {
+                System.out.println("Valor de C fora dos limites para pontuação máxima.");
+                return;
+            }
+
+            int maxDistance = findMaxDistance(graph, Ci, Di, N);
+            System.out.println("Tempo máximo de fuga: " + maxDistance);
+        }
+    }
+
+    private static int findMaxDistance(List<List<Integer> > graph, int start, int target, int N) {
+        boolean[] visited = new boolean[N];
+        return dfs(graph, start, target, visited);
+    }
+
+    private static int dfs(List<List<Integer> > graph, int current, int target, boolean[] visited) {
+        if (current == target) {
+            return 0;
+        }
+
+        visited[current - 1] = true;
+        int maxDistance = -1;
+
+        for (int neighbor : graph.get(current - 1)) {
+            if (!visited[neighbor - 1]) {
+                int distance = 1 + dfs(graph, neighbor, target, visited);
+                maxDistance = Math.max(maxDistance, distance);
+            }
+        }
+
+        visited[current - 1] = false;
+        return maxDistance;
+    }
+}
