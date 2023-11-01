9d3c0ea02835218b6935a73099cfac36dbc8505f
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-11-01 12:48:29 -0300

    Update Vestibular 1.java

--- Vestibular 1.java
+++ Vestibular 1.java
@@ -1,18 +1,20 @@
+package aulas;
+
 import java.util.Scanner;
 
-public class Main {
+public class Aulas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Número de questões: ");
-        int N = sc.nextInt(); // Número de questões
-        sc.nextLine(); // Consumir a quebra de linha
+        int N = sc.nextInt(); 
+        sc.nextLine(); 
 
         System.out.print("Gabarito da prova: ");
-        String gabarito = sc.nextLine(); // Ler o gabarito
+        String gabarito = sc.nextLine(); 
 
         System.out.print("Respostas do candidato: ");
-        String respostasCandidato = sc.nextLine(); // Ler as respostas do candidato
+        String respostasCandidato = sc.nextLine(); 
 
         int acertos = 0;
 
