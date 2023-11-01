fb163c330555c3e9f441c88b2a62a1b57f54d4d6
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-11-01 12:47:01 -0300

    Atualizar o Vestibular 1.java

--- Vestibular 1.java
+++ Vestibular 1.java
@@ -1,11 +1,27 @@
-A maioria das universidades brasileiras usa o vestibular para selecionar seus alunos. O vestibular consiste de uma ou mais provas sobre as matérias do Ensino Médio, visando avaliar os conhecimentos dos candidatos.
+import java.util.Scanner;
 
-Um formato popular de prova de vestibular é a prova objetiva. Neste formato de prova, cada candidato deve escolher uma das cinco alternativas apresentadas pela questão como sendo a correta. Durante a correção dos cartões, cada questão onde a alternativa escolhida pelo candidato é a mesma do gabarito, ele ganha um ponto. Alguns dos vestibulares mais concorridos do Brasil são disputados por dezenas de milhares de candidatos, e, por isso, geralmente usa-se uma folha de leitura óptica e um programa de computador para corrigir as provas de todos os candidatos e gerar a lista com suas pontuações.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-Você trabalha no comitê responsável pelo vestibular em uma faculdade e deve escrever um programa que, dado o gabarito e as respostas de um dos candidatos, determina o número de acertos daquele candidato.
+        System.out.print("Número de questões: ");
+        int N = sc.nextInt(); // Número de questões
+        sc.nextLine(); // Consumir a quebra de linha
 
-Entrada
-A entrada contém um único conjunto de testes, que deve ser lido do dispositivo de entrada padrão. A primeira linha da entrada contém um único inteiro N, indicando o número de questões da prova. A segunda linha da entrada contém uma cadeia de N caracteres, indicando o gabarito da prova. A terceira linha da entrada contém outra cadeia de N caracteres, indicando as opções marcadas pelo candidato. Ambas as cadeias contêm apenas os caracteres 'A', 'B', 'C', 'D' e 'E' (sempre em letra maiúscula).
+        System.out.print("Gabarito da prova: ");
+        String gabarito = sc.nextLine(); // Ler o gabarito
 
-Saída
-Seu programa deve imprimir na saída padrão uma única linha contendo um único inteiro, indicando o número de acertos do candidato.
+        System.out.print("Respostas do candidato: ");
+        String respostasCandidato = sc.nextLine(); // Ler as respostas do candidato
+
+        int acertos = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (gabarito.charAt(i) == respostasCandidato.charAt(i)) {
+                acertos++;
+            }
+        }
+
+        System.out.println("Número de acertos do candidato: " + acertos);
+    }
+}
